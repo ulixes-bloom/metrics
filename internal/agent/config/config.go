cd89a445dda480f67cf86dc51debbb594331f94d
Иванова Наталия Владимировна <36509248+CharlyLovegood@users.noreply.github.com>
2025-02-23 19:44:07 +0300

    Add json configuration file

--- internal/agent/config/config.go
+++ internal/agent/config/config.go
@@ -3,41 +3,62 @@
 package config
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
+	"dario.cat/mergo"
 	"github.com/caarlos0/env"
 )
 
 // Config holds the configuration parameters for the client.
 // Parameters can be set through environment variables or command-line flags.
 type Config struct {
-	ServerAddr     string `env:"ADDRESS"`         // Address and port of the server.
-	ReportInterval int    `env:"REPORT_INTERVAL"` // Interval for reporting metrics, in seconds.
-	PollInterval   int    `env:"POLL_INTERVAL"`   // Interval for polling metrics, in seconds.
-	RateLimit      int    `env:"RATE_LIMIT"`      // Rate limit for metric updates.
-	LogLvl         string `env:"LOGLVL"`          // Logging level (e.g., "info", "debug").
-	HashKey        string `env:"KEY"`             // Key for signing metrics data.
-	CryptoKey      string `env:"CRYPTO_KEY"`      // Public key for data encryption.
+	ServerAddr     string `env:"ADDRESS" json:"address"`                 // Address and port of the server.
+	ReportInterval int    `env:"REPORT_INTERVAL" json:"report_interval"` // Interval for reporting metrics, in seconds.
+	PollInterval   int    `env:"POLL_INTERVAL" json:"poll_interval"`     // Interval for polling metrics, in seconds.
+	RateLimit      int    `env:"RATE_LIMIT" json:"rate_limit"`           // Rate limit for metric updates.
+	LogLvl         string `env:"LOGLVL" json:"loglvl"`                   // Logging level (e.g., "info", "debug").
+	HashKey        string `env:"KEY" json:"key"`                         // Key for signing metrics data.
+	CryptoKey      string `env:"CRYPTO_KEY" json:"crypto_key"`           // Public key for data encryption.
 }
 
 // Parse parses the configuration from command-line flags and environment variables.
 func Parse() (*Config, error) {
 	var conf Config
-	defaultValues := GetDefault()
+	var err error
 
-	flag.StringVar(&conf.ServerAddr, "a", defaultValues.ServerAddr, "address and port of server")
-	flag.IntVar(&conf.ReportInterval, "r", defaultValues.ReportInterval, "metrics report interval")
-	flag.IntVar(&conf.PollInterval, "p", defaultValues.PollInterval, "metrics update interval")
-	flag.IntVar(&conf.RateLimit, "l", defaultValues.RateLimit, "rate limit for metric updates")
-	flag.StringVar(&conf.LogLvl, "ll", defaultValues.LogLvl, "logging level")
-	flag.StringVar(&conf.HashKey, "k", defaultValues.HashKey, "key to sign the metrics data")
-	flag.StringVar(&conf.CryptoKey, "crypto-key", defaultValues.HashKey, "public key for data encryption")
+	// read config from json config file
+	configFile := getConfigFileName()
+	if configFile != "" {
+		conf, err = parseConfigFromFile(configFile)
+		if err != nil {
+			return nil, fmt.Errorf("config.parse: %w", err)
+		}
+	}
+
+	// fill config empty parameters with default values
+	defaultValues := GetDefault()
+	err = mergo.Merge(&conf, defaultValues)
+	if err != nil {
+		return nil, fmt.Errorf("config.parse: %w", err)
+	}
+
+	flag.StringVar(&conf.ServerAddr, "a", conf.ServerAddr, "address and port of server")
+	flag.IntVar(&conf.ReportInterval, "r", conf.ReportInterval, "metrics report interval")
+	flag.IntVar(&conf.PollInterval, "p", conf.PollInterval, "metrics update interval")
+	flag.IntVar(&conf.RateLimit, "l", conf.RateLimit, "rate limit for metric updates")
+	flag.StringVar(&conf.LogLvl, "ll", conf.LogLvl, "logging level")
+	flag.StringVar(&conf.HashKey, "k", conf.HashKey, "key to sign the metrics data")
+	flag.StringVar(&conf.CryptoKey, "crypto-key", conf.HashKey, "public key for data encryption")
+	flag.StringVar(&configFile, "c", configFile, "json file with configuration")
 	flag.Parse()
 
-	err := env.Parse(&conf)
+	err = env.Parse(&conf)
 	if err != nil {
 		return nil, fmt.Errorf("config.parse: %w", err)
 	}
@@ -52,6 +73,7 @@ func Parse() (*Config, error) {
 		return nil, errors.New("config.parse: negative or zero rate interval")
 	}
 
+	fmt.Println(conf)
 	return &conf, nil
 }
 
@@ -68,6 +90,36 @@ func GetDefault() (conf *Config) {
 		HashKey:        "",
 		CryptoKey:      "",
 	}
+}
+
+func getConfigFileName() (configPath string) {
+	for _, arg := range os.Args[1:] {
+		if strings.HasPrefix(arg, "-c=") {
+			configPath = strings.TrimPrefix(arg, "-c=")
+		}
+		if strings.HasPrefix(arg, "-config=") {
+			configPath = strings.TrimPrefix(arg, "-config=")
+		}
+	}
+	if env, isExist := os.LookupEnv("CONFIG"); isExist {
+		configPath = env
+	}
+
+	return
+}
+
+func parseConfigFromFile(fname string) (conf Config, err error) {
+	f, err := os.Open(fname)
+	if err != nil {
+		return conf, fmt.Errorf("config.parseConfigFromFile: %w", err)
+	}
+
+	dec := json.NewDecoder(f)
+	if err := dec.Decode(&conf); err != nil {
+		return conf, fmt.Errorf("config.parseConfigFromFile: %w", err)
+	}
+
+	return conf, nil
 }
 
 // GetNormilizedServerAddr returns the server address normalized with an "http://" prefix.
--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -3,42 +3,62 @@
 package config
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
+	"dario.cat/mergo"
 	"github.com/caarlos0/env"
 )
 
 type Config struct {
-	RunAddr         string `env:"ADDRESS"`           // The address and port for the server to listen on.
-	LogLvl          string `env:"LOGLVL"`            // The logging level to be used (e.g., Info, Debug).
-	StoreInterval   int    `env:"STORE_INTERVAL"`    // Interval at which metrics are stored.
-	FileStoragePath string `env:"FILE_STORAGE_PATH"` // Path to store metrics data in a file.
-	Restore         bool   `env:"RESTORE"`           // Flag to determine if metrics should be restored from storage.
-	DatabaseDSN     string `env:"DATABASE_DSN"`      // Data source name for connecting to a PostgreSQL database.
-	HashKey         string `env:"KEY"`               // Key used for signing and validating metrics data.
-	CryptoKey       string `env:"CRYPTO_KEY"`        // Public key for data encryption.
+	RunAddr         string `env:"ADDRESS" json:"address"`                     // The address and port for the server to listen on.
+	LogLvl          string `env:"LOGLVL" json:"loglvl"`                       // The logging level to be used (e.g., Info, Debug).
+	StoreInterval   int    `env:"STORE_INTERVAL" json:"store_interval"`       // Interval at which metrics are stored.
+	FileStoragePath string `env:"FILE_STORAGE_PATH" json:"file_storage_path"` // Path to store metrics data in a file.
+	Restore         bool   `env:"RESTORE" json:"restore"`                     // Flag to determine if metrics should be restored from storage.
+	DatabaseDSN     string `env:"DATABASE_DSN" json:"database_dsn"`           // Data source name for connecting to a PostgreSQL database.
+	HashKey         string `env:"KEY" json:"hash_key"`                        // Key used for signing and validating metrics data.
+	CryptoKey       string `env:"CRYPTO_KEY" json:"crypto_key"`               // Public key for data encryption.
 }
 
 // Parse parses the configuration from command-line flags and environment variables.
 func Parse() (*Config, error) {
 	var conf Config
+	var err error
+
+	// read config from json config file
+	configFile := getConfigFileName()
+	if configFile != "" {
+		conf, err = parseConfigFromFile(configFile)
+		if err != nil {
+			return nil, fmt.Errorf("config.parse: %w", err)
+		}
+	}
+
+	// fill config empty parameters with default values
 	defaultValues := GetDefault()
+	err = mergo.Merge(&conf, defaultValues)
+	if err != nil {
+		return nil, fmt.Errorf("config.parse: %w", err)
+	}
 
-	flag.StringVar(&conf.RunAddr, "a", defaultValues.RunAddr, "address and port to run server")
-	flag.StringVar(&conf.LogLvl, "l", defaultValues.LogLvl, "logging level")
-	flag.IntVar(&conf.StoreInterval, "i", defaultValues.StoreInterval, "store interval")
-	flag.StringVar(&conf.FileStoragePath, "f", defaultValues.FileStoragePath, "file storage path")
-	flag.BoolVar(&conf.Restore, "r", defaultValues.Restore, "to restore metrics data")
-	flag.StringVar(&conf.DatabaseDSN, "d", defaultValues.DatabaseDSN, "Postgresql data source name")
-	flag.StringVar(&conf.HashKey, "k", defaultValues.HashKey, "key to sign the metrics data")
-	flag.StringVar(&conf.CryptoKey, "crypto-key", defaultValues.HashKey, "public key for data encryption")
-
+	flag.StringVar(&conf.RunAddr, "a", conf.RunAddr, "address and port to run server")
+	flag.StringVar(&conf.LogLvl, "l", conf.LogLvl, "logging level")
+	flag.IntVar(&conf.StoreInterval, "i", conf.StoreInterval, "store interval")
+	flag.StringVar(&conf.FileStoragePath, "f", conf.FileStoragePath, "file storage path")
+	flag.BoolVar(&conf.Restore, "r", conf.Restore, "to restore metrics data")
+	flag.StringVar(&conf.DatabaseDSN, "d", conf.DatabaseDSN, "Postgresql data source name")
+	flag.StringVar(&conf.HashKey, "k", conf.HashKey, "key to sign the metrics data")
+	flag.StringVar(&conf.CryptoKey, "crypto-key", conf.HashKey, "public key for data encryption")
+	flag.StringVar(&configFile, "c", configFile, "json file with configuration")
 	flag.Parse()
 
-	err := env.Parse(&conf)
+	err = env.Parse(&conf)
 	if err != nil {
 		return nil, fmt.Errorf("config.parse: %w", err)
 	}
@@ -47,7 +67,38 @@ func Parse() (*Config, error) {
 		return nil, errors.New("config.parse: negative store interval")
 	}
 
+	fmt.Println(conf)
 	return &conf, nil
+}
+
+func getConfigFileName() (configPath string) {
+	for _, arg := range os.Args[1:] {
+		if strings.HasPrefix(arg, "-c=") {
+			configPath = strings.TrimPrefix(arg, "-c=")
+		}
+		if strings.HasPrefix(arg, "-config=") {
+			configPath = strings.TrimPrefix(arg, "-config=")
+		}
+	}
+	if env, isExist := os.LookupEnv("CONFIG"); isExist {
+		configPath = env
+	}
+
+	return
+}
+
+func parseConfigFromFile(fname string) (conf Config, err error) {
+	f, err := os.Open(fname)
+	if err != nil {
+		return conf, fmt.Errorf("config.parseConfigFromFile: %w", err)
+	}
+
+	dec := json.NewDecoder(f)
+	if err := dec.Decode(&conf); err != nil {
+		return conf, fmt.Errorf("config.parseConfigFromFile: %w", err)
+	}
+
+	return conf, nil
 }
 
 // GetDefault returns a Config object populated with default values for all
